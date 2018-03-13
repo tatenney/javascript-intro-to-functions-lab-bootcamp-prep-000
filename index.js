d97faa97e63d88ae07dd8db62a5ee9a967f61a1a
tainotenney@gmail.com <tainotenney@gmail.com>
2018-03-13 20:31:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+function logShout()
+{
+console.log("HELLO")
+}
+
+function logWhisper()
+{
+console.log("hello")
+}
+
+function sayHiToGrandma()
+{
+console.log("I love you, Grandma.")
+return ("I love you, too")
+}
+
